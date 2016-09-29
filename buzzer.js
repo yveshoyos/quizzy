94efe56899e962d851cd924ba0167f610124f391
Vincent Dieltiens <and1hotsauce@gmail.com>
2016-09-30 00:04:07 +0200

    Use ES6 for buzzer

--- buzzer.js
+++ buzzer.js
@@ -1,30 +1,32 @@
-var _ = require('underscore')
+'use strict';
+
+var _ = require('underscore');
 
 var signals = [
-		// controller 1
-		[0, 0, 1, 0, 240],
-		[0, 0, 16, 0, 240],
-		[0, 0, 8, 0, 240],
-		[0, 0, 4, 0, 240],
-		[0, 0, 2, 0, 240],
-		// controller 2
-		[0, 0, 32, 0, 240],
-		[0, 0, 0, 2, 240],
-		[0, 0, 0, 1, 240],
-		[0, 0, 128, 0, 240],
-		[0, 0, 64, 0, 240],
-		// controller 3
-		[0, 0, 0, 4, 240],
-		[0, 0, 0, 64, 240],
-		[0, 0, 0, 32, 240],
-		[0, 0, 0, 16, 240],
-		[0, 0, 0, 8, 240],
-		// controller 4
-		[0, 0, 0, 128, 240],
-		[0, 0, 0, 0, 248],
-		[0, 0, 0, 0, 244],
-		[0, 0, 0, 0, 242],
-		[0, 0, 0, 0, 241]
+	// controller 1
+	[0, 0, 1, 0, 240],
+	[0, 0, 16, 0, 240],
+	[0, 0, 8, 0, 240],
+	[0, 0, 4, 0, 240],
+	[0, 0, 2, 0, 240],
+	// controller 2
+	[0, 0, 32, 0, 240],
+	[0, 0, 0, 2, 240],
+	[0, 0, 0, 1, 240],
+	[0, 0, 128, 0, 240],
+	[0, 0, 64, 0, 240],
+	// controller 3
+	[0, 0, 0, 4, 240],
+	[0, 0, 0, 64, 240],
+	[0, 0, 0, 32, 240],
+	[0, 0, 0, 16, 240],
+	[0, 0, 0, 8, 240],
+	// controller 4
+	[0, 0, 0, 128, 240],
+	[0, 0, 0, 0, 248],
+	[0, 0, 0, 0, 244],
+	[0, 0, 0, 0, 242],
+	[0, 0, 0, 0, 241]
 ]
 
 function signalsEqual(a, b) {
@@ -55,30 +57,30 @@ function callHandlers(handlers, controllerIndex, buttonIndex) {
 	}
 }
 
-function Buzzer(device) {
-	this.device = device
-	this.lights = [0x0, 0x0, 0x0, 0x0]
+class Buzzer {
+	constructor(device) {
+		this.device = device
+		this.lights = [0x0, 0x0, 0x0, 0x0]
 
-	this.handlers = [];
-	this.device.on("data", function(signal) {
-		var signalIndex = searchSignalIndex(signal);
-		if (signalIndex >= 0) {
-			var controllerIndex = parseInt(signalIndex / 5, 10);
-			var buttonIndex = signalIndex % 5;
+		this.handlers = [];
+		this.device.on("data", (signal) => {
+			var signalIndex = searchSignalIndex(signal);
+			if (signalIndex >= 0) {
+				var controllerIndex = parseInt(signalIndex / 5, 10);
+				var buttonIndex = signalIndex % 5;
 
-			var key = 'c'+controllerIndex+'b'+buttonIndex;
-			callHandlers(this.handlers[key], controllerIndex, buttonIndex);
+				var key = 'c'+controllerIndex+'b'+buttonIndex;
+				callHandlers(this.handlers[key], controllerIndex, buttonIndex);
 
-			key = 'c'+controllerIndex;
-			callHandlers(this.handlers[key], controllerIndex, buttonIndex);
+				key = 'c'+controllerIndex;
+				callHandlers(this.handlers[key], controllerIndex, buttonIndex);
 
-			callHandlers(this.handlers['all'], controllerIndex, buttonIndex);
-		}
-	}.bind(this))
-}
+				callHandlers(this.handlers['all'], controllerIndex, buttonIndex);
+			}
+		});
+	}
 
-Buzzer.prototype = {
-	light: function(controllerIndexes, value) {
+	light(controllerIndexes, value) {
 		if (value != 0x00 && value != 0xFF) {
 			throw new Error("light should have a value of 0x0 or 0xFF")
 		}
@@ -98,14 +100,17 @@ Buzzer.prototype = {
 		}
 
 		this.device.write(message);
-	},
-	lightOn: function(controllerIndexes) {
+	}
+
+	lightOn(controllerIndexes) {
 		this.light(controllerIndexes, 0xFF);
-	},
-	lightOff: function(controllerIndexes) {
+	}
+
+	lightOff(controllerIndexes) {
 		this.light(controllerIndexes, 0x0);
-	},
-	blink: function(controllerIndexes, times, duration) {
+	}
+
+	blink(controllerIndexes, times, duration) {
 		var duration = duration || 150;
 		var times = times || 5;
 		var on = true
@@ -124,9 +129,10 @@ Buzzer.prototype = {
 
 			on = !on;
 
-		}.bind(this), duration)
-	},
-	onPress: function(callback, controllerIndex, buttonIndex) {
+		}, duration);
+	}
+
+	onPress(callback, controllerIndex, buttonIndex) {
 		var key = 'all';
 		if (controllerIndex != undefined || buttonIndex != undefined) {
 			key = '';
@@ -138,19 +144,16 @@ Buzzer.prototype = {
 			}
 		}
 
-		console.log('===>', key)
 		if (!(key in this.handlers)) {
 			this.handlers[key] = [];
 		}
 		this.handlers[key].push(callback);
-		return function() {
+		return () => {
 			var index = this.handlers[key].indexOf(callback);
-			console.log('try to remove...', index)
 			if (index >= 0) {
-				console.log('remooooooo')
 				this.handlers[key].splice(index, 1);
 			}
-		}.bind(this);
+		};
 	}
 }
 
